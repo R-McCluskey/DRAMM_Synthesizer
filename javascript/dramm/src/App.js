cd420b37819becaa09684780b01bcb55a7723bfa
Matthew David Wasylko <matthew.wasylko@gmail.com>
2023-12-01 12:37:10 +0000

    start button changed

--- javascript/dramm/src/App.js
+++ javascript/dramm/src/App.js
@@ -1,14 +1,21 @@
 import SynthContainer from './containers/SynthContainer'
 
+import styled from 'styled-components';
+
+const AppStyle = styled.div`
+overflow: hidden;
+overscroll-behavior: none;
+`
+
 
 const App = () => {
 
   document.body.classList.add("no-scroll")
   
   return (
-    <body>
+    <AppStyle>
     <SynthContainer/>
-    </body>
+    </AppStyle>
   );
 }
 
